2f54390b2c390632ffbeadd0374f5d2f17b18a57
LWJ9527 <2311349863@qq.com>
2019-01-09 11:11:02 +0800

    take a sort

---
+++ Leetcode/leetcode27.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int size = nums.size();
+        int sum = 0;
+        for(int i = 0;i < size;i++)
+        {
+            while(size-1 > i && nums[size-1] == val) size--,sum++;
+            if(size > 0 && nums[i] == val) nums[i] = nums[--size],sum++;
+        }
+        return nums.size()-sum;
+    }
+};
